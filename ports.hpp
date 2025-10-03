39de7a03e09d8e9210daee23b639f5ec675bdf1c
ayanprime <86917904+ayanprime@users.noreply.github.com>
2025-10-02 23:38:34 -0400

    Create ports.hpp

---
+++ ports.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "main.h"
+
+extern brain Brain;
+extern controller Controller1;
+
+extern motor rmf;
+extern motor rmb;
+extern motor_group Right;
+extern motor lmf;
+extern motor lmb;
+extern motor_group Left;
+
+extern digital_out descore;
+extern rotation PodLeft;
+extern rotation PodRight;
