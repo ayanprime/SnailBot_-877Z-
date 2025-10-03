b17059e2c2049dd4f57864e6a10d23e5fcb87f7b
ayanprime <86917904+ayanprime@users.noreply.github.com>
2025-10-02 23:36:13 -0400

    Create Driver.cpp

---
+++ Driver.cpp
@@ -0,0 +1,104 @@
+#include "main.h"
+#include "Driver.hpp"
+
+
+// driving joysticks 
+// goal slelections 
+// int Level = 1;
+/*
+
+void Andrew() {
+    Left.setVelocity(Controller1.Axis3.position() + Controller1.Axis1.position(), percent);
+    Right.setVelocity(Controller1.Axis3.position() - Controller1.Axis1.position(), percent);
+    Left.spin(forward);
+    Right.spin(forward);
+
+//wants arrows for goal selection
+}
+
+
+void Ayan() {
+
+    Left.setVelocity(Controller1.Axis3.position() + Controller1.Axis1.position(), percent);
+    Right.setVelocity(Controller1.Axis3.position() - Controller1.Axis1.position(), percent);
+    
+
+     JUST A BASE, WILL BE CHANGED: (also this only occurs when the up or down button is being pressed, this will need to be changed to only press once)
+    if(Controller1.ButtonUp.pressing && Level == 1())
+    {
+    level.spin(forward, 100, percent); 
+    level2.spin(reverse, 100, percent);
+    Level+=1;
+    }
+
+    if(Controller1.ButtonDown.pressing && Level == 2 || Controller1.ButtonDown.pressing && Level == 3 ) {
+    
+    //FINISH/correct others later as this code is very unusable right now. 
+    
+    
+    }
+    
+}
+
+
+void Alivia() {
+
+
+
+
+
+}
+
+
+void Brian() {
+    Left.setVelocity(Controller1.Axis3.position() + Controller1.Axis1.position(), percent);
+    Right.setVelocity(Controller1.Axis3.position() - Controller1.Axis1.position(), percent);
+    Left.spin(forward);
+    Right.spin(forward);
+
+
+  
+
+}
+
+void Bennet() {
+  
+    
+
+}
+
+
+void Connor() {
+
+
+
+}
+
+
+void Grace() {
+
+
+
+
+
+}
+
+
+void Maria() {
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+*/
