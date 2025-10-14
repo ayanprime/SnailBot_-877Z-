b802e319b6c90fa3e283e8b4144e992cfc079e9b
ayanprime <86917904+ayanprime@users.noreply.github.com>
2025-10-13 21:48:59 -0400

    Update ports.cpp

--- ports.cpp
+++ ports.cpp
@@ -5,17 +5,23 @@ brain Brain;
 controller Controller1 = controller(primary);
 
 //motors
-motor rmf = motor(PORT1, ratio18_1, true);
-motor rmb = motor(PORT2, ratio18_1, true);
+motor rmf = motor(PORT1, ratio18_1, false);
+motor rmb = motor(PORT2, ratio18_1, false);
 motor_group Right = motor_group(rmf, rmb);
 
-motor lmf = motor(PORT3, ratio18_1, false);
-motor lmb = motor(PORT4, ratio18_1, false);
+motor lmf = motor(PORT3, ratio18_1, true);
+motor lmb = motor(PORT4, ratio18_1, true);
 motor_group Left = motor_group(lmf, lmb);
 
+motor intake = motor(PORT5, ratio18_1, false);
+
 //pods
-rotation PodLeft = rotation(PORT5, false);
-rotation PodRight = rotation(PORT6, true);
+rotation PodLeft = rotation(PORT5, true);
+rotation PodRight = rotation(PORT6, false);
 
 //pistons
 digital_out descore = digital_out(Brain.ThreeWirePort.A);
+digital_out rPiston = digital_out(Brain.ThreeWirePort.B);
+digital_out lPiston = digital_out(Brain.ThreeWirePort.C);
+digital_out trap = digital_out(Brain.ThreeWirePort.D);
+
