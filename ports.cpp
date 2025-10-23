cbe0b4e657d98eb4bd964e0500c696f9bcacd789
ayanprime <86917904+ayanprime@users.noreply.github.com>
2025-10-22 20:36:07 -0400

    Define motors and devices in ports.cpp
    Initialized motors and devices with specific ports.

--- ports.cpp
+++ ports.cpp
@@ -1,23 +1,27 @@
-#pragma once
 #include "main.h"
 
-extern brain Brain;
-extern controller Controller1;
+//main devices
+brain Brain;
+controller Controller1 = controller(primary);
 
-extern motor rmf;
-extern motor rmb;
-extern motor_group Right;
-extern motor lmf;
-extern motor lmb;
-extern motor_group Left;
+//motors
+motor rmf = motor(PORT19, ratio18_1, false);
+motor rmb = motor(PORT20, ratio18_1, false);
+motor_group Right = motor_group(rmf, rmb);
 
-extern motor intake;
+motor lmf = motor(PORT12, ratio18_1, true);
+motor lmb = motor(PORT11, ratio18_1, true);
+motor_group Left = motor_group(lmf, lmb);
 
-extern motor fingerer;
+motor intake = motor(PORT5, ratio18_1, false);
 
-extern digital_out rPiston;
-extern digital_out lPiston;
-extern digital_out trap;
+motor converter = motor(PORT9, ratio18_1, false);
 
-extern rotation PodLeft;
-extern rotation PodRight;
+motor conveyor = motor(PORT10, ratio18_1, false);
+
+motor fingerer = motor(PORT8, ratio18_1, false);
+
+//pods
+rotation PodLeft = rotation(PORT13, true);
+rotation PodRight = rotation(PORT18, false);
+
