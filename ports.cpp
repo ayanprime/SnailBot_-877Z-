134ca3ad593559d914be0e201350e1b26ad579fd
Ayan Bhowmik <86917904+ayanprime@users.noreply.github.com>
2026-01-21 14:38:55 -0500

    Change motor and rotation port assignments
    Updated motor ports for various components including motors and rotations.

--- ports.cpp
+++ ports.cpp
@@ -5,24 +5,20 @@ brain Brain;
 controller Controller1 = controller(primary);
 
 //motors
-motor rmf = motor(PORT19, ratio18_1, false);
+motor rmf = motor(PORT10, ratio18_1, false);
 motor rmb = motor(PORT20, ratio18_1, false);
 motor_group Right = motor_group(rmf, rmb);
 
-motor lmf = motor(PORT12, ratio18_1, true);
+motor lmf = motor(PORT1, ratio18_1, true);
 motor lmb = motor(PORT11, ratio18_1, true);
 motor_group Left = motor_group(lmf, lmb);
 
-motor intake = motor(PORT8, ratio18_1, false);
-
-motor converter = motor(PORT7, ratio18_1, false);
+motor intake = motor(PORT19, ratio18_1, false);
 
 motor conveyor = motor(PORT9, ratio18_1, true);
 
-motor fingerer = motor(PORT1, ratio18_1, false);
-
-motor top = motor(PORT17, ratio18_1, false);
+motor fingerer = motor(PORT2, ratio18_1, false);
 
 //pods
-rotation PodLeft = rotation(PORT13, true);
-rotation PodRight = rotation(PORT18, false);
+rotation PodLeft = rotation(PORT13, false);
+rotation PodRight = rotation(PORT15, false);
