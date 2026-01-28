8258e70b28d5a96944be614cae1d1a07084ca7ce
Ayan Bhowmik <86917904+ayanprime@users.noreply.github.com>
2026-01-28 14:57:47 -0500

    Update auton.cpp with new auto routines
    Refactor autonomous routines and add new skills auto function.

--- auton.cpp
+++ auton.cpp
@@ -1,10 +1,11 @@
 #include "main.h"
+#include <Driver.hpp>
 #ifndef PI
 #define PI 3.14159265358979323846
 #endif
 double wheelrad = 1.25; //inches
 double wheelcirc = 2 * PI * wheelrad; //inches
-
+//hi
 double Rerror;
 double Lerror;
 double Rintegral = 0;
@@ -82,6 +83,67 @@ void resetPods() {
 }
 
 void LRBauto() {
+// move 1
+    FWD(22.549, 30);
+    wait(0.5, sec);
+    TRN(-90, 30);
+    wait(0.5, sec);
+    //move 2
+    FWD(7.342, 30);
+    wait(0.5, sec);
+    TRN(90, 30);
+    wait(0.5, sec);
+    // move 3
+    FWD(16.75, 30);
+    wait(0.5, sec);
+    TRN(90, 30);
+    wait(0.5, sec);
+    // move 4: intake blocks
+    level = 4;
+    Level();
+    FWD(29, 30);
+    top.stop();
+    conveyor.stop();
+    intake.stop();
+    convertor.stop();
+    wait(0.5, sec);
+    TRN(90, 30);
+    wait(0.5, sec);
+    // move 5
+    FWD(24, 30);
+    wait(0.5, sec);
+    TRN(-90, 30);
+    wait(0.5, sec);
+    //move 6
+    FWD(17.25, 30);
+    wait(0.5, sec);
+    TRN(90, 30);
+    wait(0.5, sec);
+    // move 7: go to match loader
+    stickBenis.set(true);
+    Level();
+    FWD(10, 30);
+    wait(15, sec);
+    top.stop();
+    conveyor.stop();
+    intake.stop();
+    convertor.stop();
+    stickBenis.set(false);
+    FWD(-10, 30);
+    wait(0.5, sec);
+    TRN(180, 30); 
+    wait(0.5, sec);
+    // move 8: go to score
+    FWD(10, 30);
+    wait(0.5, sec);
+    level = 3;
+    Level();
+
+    
+    
+}
+
+void LRWPauto(){
 
     intake.setVelocity(100, percent);
     conveyor.setVelocity(100, percent);
@@ -120,122 +182,306 @@ void LRBauto() {
 
 
     resetPods();
-    
+
 }
 
 void RRBauto(){
-
-    intake.setVelocity(100, percent);
-    conveyor.setVelocity(70, percent);
-
-    while(move1) {
-
-        Right.setVelocity(RPIDpwr(3.55), percent);
-        Left.setVelocity(LPIDpwr(3.55), percent);
-
-        Right.spin(forward);
-        Left.spin(forward);
-
-        if(Right.velocity(percent) == 0 && Left.velocity(percent) == 0 && PodRight.position(degrees) != 0 && PodLeft.position(degrees) != 0) {
-
-            wait(15, msec);
-
-
-            move1 = false;
-
-        }
-
-    }
-
-    Right.setVelocity(70, percent);
-    Left.setVelocity(70, percent);
-
-    Left.spinFor(-1.5, turns, false);
-    Right.spinFor(1.5, turns, true);
-
-    resetPods();
-
-    while(move2) {
-
-        intake.spin(forward);
-        conveyor.spin(forward);
-
-        Right.setVelocity(RPIDpwr(27.15), percent);
-        Left.setVelocity(LPIDpwr(27.15), percent);
-
-        Right.spin(forward);
-        Left.spin(forward);
-
-        if(Right.velocity(percent) == 0 && Left.velocity(percent) == 0 && PodRight.position(degrees) != 0 && PodLeft.position(degrees) != 0) {
-
-            wait(15, msec);
-
-
-            move2 = false;
-
-        }
-
-    }
-
-    resetPods();
-
-    while(move3) {
-
-        intake.spin(forward);
-        conveyor.spin(forward);
-
-        Right.setVelocity(RPIDpwr(-4.4), percent);
-        Left.setVelocity(LPIDpwr(-4.4), percent);
-
-        Right.spin(forward);
-        Left.spin(forward);
-
-        if(Right.velocity(percent) == 0 && Left.velocity(percent) == 0 && PodRight.position(degrees) != 0 && PodLeft.position(degrees) != 0) {
-
-            wait(15, msec);
+// move 1
+    FWD(22.549, 30);
+    wait(0.5, sec);
+    TRN(90, 30);
+    wait(0.5, sec);
+    //move 2
+    FWD(7.342, 30);
+    wait(0.5, sec);
+    TRN(-90, 30);
+    wait(0.5, sec);
+    // move 3
+    FWD(16.75, 30);
+    wait(0.5, sec);
+    TRN(-90, 30);
+    wait(0.5, sec);
+    // move 4: intake blocks
+    level = 4;
+    Level();
+    FWD(29, 30);
+    top.stop();
+    conveyor.stop();
+    intake.stop();
+    convertor.stop();
+    wait(0.5, sec);
+    TRN(-90, 30);
+    wait(0.5, sec);
+    // move 5
+    FWD(24, 30);
+    wait(0.5, sec);
+    TRN(90, 30);
+    wait(0.5, sec);
+    //move 6
+    FWD(17.25, 30);
+    wait(0.5, sec);
+    TRN(-90, 30);
+    wait(0.5, sec);
+    // move 7: go to match loader
+    stickBenis.set(true);
+    Level();
+    FWD(10, 30);
+    wait(15, sec);
+    top.stop();
+    conveyor.stop();
+    intake.stop();
+    convertor.stop();
+    stickBenis.set(false);
+    FWD(-10, 30);
+    wait(0.5, sec);
+    TRN(180, 30); 
+    wait(0.5, sec);
+    // move 8: go to score
+    FWD(10, 30);
+    wait(0.5, sec);
+    level = 3;
+    Level();
 
 
-            move3 = false;
 
-        }
+}
 
-    }
+void RRWPauto(){
+     
+    
 
-    Right.setVelocity(70, percent);
-    Left.setVelocity(70, percent);
+}
 
-    Left.spinFor(-0.75, turns, false);
-    Right.spinFor(0.75, turns, true);
+void LBBauto(){
 
-    resetPods();
+  // move 1
+    FWD(22.549, 30);
+    wait(0.5, sec);
+    TRN(-90, 30);
+    wait(0.5, sec);
+    //move 2
+    FWD(7.342, 30);
+    wait(0.5, sec);
+    TRN(90, 30);
+    wait(0.5, sec);
+    // move 3
+    FWD(16.75, 30);
+    wait(0.5, sec);
+    TRN(90, 30);
+    wait(0.5, sec);
+    // move 4: intake blocks
+    level = 4;
+    Level();
+    FWD(29, 30);
+    top.stop();
+    conveyor.stop();
+    intake.stop();
+    convertor.stop();
+    wait(0.5, sec);
+    TRN(90, 30);
+    wait(0.5, sec);
+    // move 5
+    FWD(24, 30);
+    wait(0.5, sec);
+    TRN(-90, 30);
+    wait(0.5, sec);
+    //move 6
+    FWD(17.25, 30);
+    wait(0.5, sec);
+    TRN(90, 30);
+    wait(0.5, sec);
+    // move 7: go to match loader
+    stickBenis.set(true);
+    Level();
+    FWD(10, 30);
+    wait(15, sec);
+    top.stop();
+    conveyor.stop();
+    intake.stop();
+    convertor.stop();
+    stickBenis.set(false);
+    FWD(-10, 30);
+    wait(0.5, sec);
+    TRN(180, 30); 
+    wait(0.5, sec);
+    // move 8: go to score
+    FWD(10, 30);
+    wait(0.5, sec);
+    level = 3;
+    Level();
 
-    while(move4) {
+    
 
-        intake.spin(reverse);
-        conveyor.spin(reverse);
+}
 
-        Right.setVelocity(RPIDpwr(11), percent);
-        Left.setVelocity(LPIDpwr(11), percent);
+void LBWPauto(){
 
-        Right.spin(forward);
-        Left.spin(forward);
-
-
-    }
+    
 
 }
 
 void RBBauto(){
 
-    FWD(5.5, 70);
+    // move 1
+    FWD(22.549, 30);
+    wait(0.5, sec);
+    TRN(90, 30);
+    wait(0.5, sec);
+    //move 2
+    FWD(7.342, 30);
+    wait(0.5, sec);
+    TRN(-90, 30);
+    wait(0.5, sec);
+    // move 3
+    FWD(16.75, 30);
+    wait(0.5, sec);
+    TRN(-90, 30);
+    wait(0.5, sec);
+    // move 4: intake blocks
+    level = 4;
+    Level();
+    FWD(29, 30);
+    top.stop();
+    conveyor.stop();
+    intake.stop();
+    convertor.stop();
+    wait(0.5, sec);
+    TRN(-90, 30);
+    wait(0.5, sec);
+    // move 5
+    FWD(24, 30);
+    wait(0.5, sec);
+    TRN(90, 30);
+    wait(0.5, sec);
+    //move 6
+    FWD(17.25, 30);
+    wait(0.5, sec);
+    TRN(-90, 30);
+    wait(0.5, sec);
+    // move 7: go to match loader
+    stickBenis.set(true);
+    Level();
+    FWD(10, 30);
+    wait(15, sec);
+    top.stop();
+    conveyor.stop();
+    intake.stop();
+    convertor.stop();
+    stickBenis.set(false);
+    FWD(-10, 30);
+    wait(0.5, sec);
+    TRN(180, 30); 
+    wait(0.5, sec);
+    // move 8: go to score
+    FWD(10, 30);
+    wait(0.5, sec);
+    level = 3;
+    Level();
 
-    wait(1, sec); 
+}
 
-    FWD(-5.5, 60);
-    wait(1, sec); 
-    TRN(-90, 70); 
-    wait(1, sec); 
-    TRN(180, 70);
-    wait(1, sec); 
-    TRN(-90, 10);
+void RBWPauto(){
+
+    
+
+}
+
+void Skillsauto(){
+    
+    FWD(15.299, 30); 
+    wait(0.5, sec); 
+    TRN(-90, 30); 
+    wait(0.5, sec); 
+    FWD(28.908, 30);
+    wait(0.5, sec); 
+    TRN(-90, 30); 
+    wait(0.5, sec); 
+    stickBenis.set(true); 
+    level = 4; 
+    Level(); 
+    FWD(9.753, 30); 
+    wait(5, sec); 
+    stickBenis.set(false);
+    top.stop();
+    conveyor.stop();
+    intake.stop();
+    convertor.stop();
+    FWD(-9.75, 30); 
+    wait(0.5, sec); 
+    TRN(90, 30); 
+    wait(0.5, sec); 
+    FWD(12, 30); 
+    wait(0.5, sec); 
+    TRN(90, 30); 
+    wait(0.5, sec); 
+    FWD(91.25, 30); 
+    wait(0.5, sec); 
+    TRN(90, 30); 
+    wait(0.5, sec); 
+    FWD(12, 30); 
+    wait(0.5, sec); 
+    TRN(-90, 30); 
+    wait(0.5, sec); 
+    stickBenis.set(true); 
+    Level(); 
+    FWD(12.75, 30); 
+    wait(5, sec); 
+    stickBenis.set(false);
+    top.stop();
+    conveyor.stop();
+    intake.stop();
+    convertor.stop();
+    FWD(-12.75, 30); 
+    wait(0.5, sec); 
+    TRN(90, 30); 
+    wait(0.5, sec); 
+    FWD(94.25, 30); 
+    wait(0.5, sec); 
+    TRN(-90, 30); 
+    wait(0.5, sec); 
+    stickBenis.set(true); 
+    Level(); 
+    FWD(12.75, 30); 
+    wait(5, sec); 
+    stickBenis.set(false);
+    top.stop();
+    conveyor.stop();
+    intake.stop();
+    convertor.stop();
+    FWD(-12.75, 30); 
+    wait(0.5, sec); 
+    TRN(90, 30); 
+    wait(0.5, sec); 
+    FWD(12, 30); 
+    wait(0.5, sec); 
+    TRN(90, 30); 
+    wait(0.5, sec);
+    FWD(91.25, 30); 
+    wait(0.5, sec);
+    TRN(90, 30); 
+    wait(0.5, sec);
+    FWD(12, 30); 
+    wait(0.5, sec);
+    TRN(-90, 30); 
+    wait(0.5, sec);
+    stickBenis.set(true); 
+    Level(); 
+    FWD(9.75, 30); 
+    wait(5, sec); 
+    stickBenis.set(false);
+    top.stop();
+    conveyor.stop();
+    intake.stop();
+    convertor.stop();
+    FWD(-20, 30); 
+    wait(0.5, sec); 
+    TRN(90, 30); 
+    wait(0.5, sec);
+    FWD(47.25, 30); 
+    wait(0.5, sec);
+    TRN(-90, 30); 
+    wait(0.5, sec);
+    FWD(9999999, 30); 
+    wait(0.5, sec);
+    
 }
