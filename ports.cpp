1da2cc0e30f00c076553f5f3f1ff2c00b39c4b4b
Ayan Bhowmik <86917904+ayanprime@users.noreply.github.com>
2026-02-06 17:28:50 -0500

    Update motor ports and add new digital output

--- ports.cpp
+++ ports.cpp
@@ -5,20 +5,21 @@ brain Brain;
 controller Controller1 = controller(primary);
 
 //motors
-motor rmf = motor(PORT10, ratio18_1, false);
+motor rmf = motor(PORT17, ratio18_1, false);
 motor rmb = motor(PORT20, ratio18_1, false);
 motor_group Right = motor_group(rmf, rmb);
 
-motor lmf = motor(PORT1, ratio18_1, true);
-motor lmb = motor(PORT11, ratio18_1, true);
+motor lmf = motor(PORT11, ratio18_1, true);
+motor lmb = motor(PORT12, ratio18_1, true);
 motor_group Left = motor_group(lmf, lmb);
 
 motor intake = motor(PORT19, ratio18_1, false);
-motor conveyor = motor(PORT9, ratio18_1, true);
-motor convertor = motor(PORT2, ratio18_1, false);
-motor top = motor(PORT12, ratio18_1, false);
+motor conveyor = motor(PORT10, ratio18_1, true);
+motor convertor = motor(PORT1, ratio18_1, false);
+motor top = motor(PORT18, ratio18_1, false);
 
 digital_out stickBenis = digital_out(Brain.ThreeWirePort.A);
+digital_out flipper = digital_out(Brain.ThreeWirePort.B);
 
 motor fingerer = motor(PORT13, ratio18_1, false);
 
